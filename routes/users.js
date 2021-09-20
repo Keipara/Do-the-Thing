4dbba1c675712b20cb35bd0cbbcdf96322c04340
Your Name <email@example.com>
2021-09-20 16:13:49 -0700

    coded user validation function

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,63 @@
 var express = require('express');
+const { check, validationResult } = require('express-validator');
+const { csrfProtection, asyncHandler } = require('./utils');
 var router = express.Router();
 
+const userValidators = [
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for First Name')
+    .isLength({ max: 30 })
+    .withMessage('First Name must not be more than 30 characters long'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Last Name')
+    .isLength({ max: 30 })
+    .withMessage('Last Name must not be more than 30 characters long'),
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address')
+    .isLength({ max: 50 })
+    .withMessage('Email Address must not be more than 50 characters long')
+    .isEmail()
+    .withMessage('Email Address is not a valid email')
+    .custom((value) => {
+      return db.User.findOne({ where: { emailAddress: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Email Address is already in use by another account');
+          }
+        });
+    }),
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for username')
+    .isLength({ max: 30 })
+    .withMessage('Username must not be more than 30 characters long'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password');
+      }
+      return true;
+    })
+];
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', csrfProtection, function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* POST */
+router.post('/signup', csrfProtection, userValidators, asyncHandler(async(req,res) => {
+
+}))
 
 module.exports = router;
