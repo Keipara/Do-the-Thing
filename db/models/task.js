c39d9c2129e8a7e9d6a268b3e2164a6e1b075cee
Nevin Chow <nevinchow@gmail.com>
2021-09-20 13:04:02 -0700

    finished setting up models and migrations

---
+++ db/models/task.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Task = sequelize.define('Task', {
+    name: DataTypes.STRING,
+    due: DataTypes.DATE,
+    complete: DataTypes.BOOLEAN,
+    listId: DataTypes.INTEGER,
+    description: DataTypes.TEXT
+  }, {});
+  Task.associate = function(models) {
+    // associations can be defined here
+  };
+  return Task;
+};
