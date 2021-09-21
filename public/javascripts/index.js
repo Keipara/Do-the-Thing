9ae5f2a9883e11c211442df78d04ab2ef010e4e3
Nevin Chow <nevinchow@gmail.com>
2021-09-21 15:52:42 -0700

    coded api fetch for task update on list click

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,25 @@
 window.addEventListener("load", (event)=>{
     console.log("hello from javascript!")
 })
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+      const res = await fetch("/tasks/task-list");
+      const { tasks } = await res.json();
+      const taskContainer = document.querySelector(".task-list");
+
+      const taskHtml = tasks.map(
+          ( task ) =>
+          `
+            <div class="card">
+          <div class="card-body">
+          <p class="card-text">${task.name}</p>
+          </div>
+          </div>
+          `
+          );
+      taskContainer.innerHTML = taskHtml.join("");
+    } catch (e) {
+      console.error(e);
+    }
+  });
