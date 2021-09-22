dcfbffafb35a94d7c61dc0714397416c7aa72476
lpriya <prakashlakshmipriya@gmail.com>
2021-09-22 00:35:17 -0700

    refactored search bar code to account for no results found and empty search value

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,28 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     let searchButton = document.getElementById("search-button");
+
         searchButton.addEventListener("click", async (event) => {
             let searchBox = document.getElementById("search-box");
             let searchTerm = searchBox.value;
-    
-            try {
-                const res = await fetch(`/tasks/search/${searchTerm}`);
-                const { tasks } = await res.json();
-                const taskContainer = document.querySelector(".task-list");
 
-                const taskHtml = tasks.map(
-                    ( task ) =>
-                    `
-                      <div class="card">
-                    <div class="card-body">
-                    <p class="card-text">${task.name}</p>
-                    </div>
-                    </div>
-                    `
-                    );
-                taskContainer.innerHTML = taskHtml.join("");
-              } catch (e) {
-                console.error(e);
-              }
+            if(searchTerm) {
+                try {
+                    const res = await fetch(`/tasks/search/${searchTerm}`);
+                    const { tasks } = await res.json();
+                    const taskContainer = document.querySelector(".task-list");
+
+                    if(tasks.length !== 0) {
+                        const taskHtml = tasks.map(
+                            ( task ) =>
+                            `
+                              <div class="card">
+                            <div class="card-body">
+                            <p class="card-text">${task.name}</p>
+                            </div>
+                            </div>
+                            `
+                            );
+                        taskContainer.innerHTML = taskHtml.join("");
+                    } else {
+                        taskContainer.innerHTML = 'Sorry! No results found.'
+                    }
+
+                  } catch (e) {
+                    console.error(e);
+                  }
+            }
 
         });
     })
