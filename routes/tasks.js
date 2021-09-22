d83dad40a402651d3bb95dd475e80624e2a668e5
Your Name <email@example.com>
2021-09-22 09:02:43 -0700

    commented out post for lists

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db/models')
-const { Task } = db;
+const { Task, List } = db;
 const {asyncHandler, handleValidationErrors} = require("./utils");
 
 router.get("/", asyncHandler(async (req, res) => {
@@ -17,6 +17,22 @@ router.get("/task-list", asyncHandler(async (req, res) => {
 })
 );
 
+router.get("/list-list", asyncHandler(async (req, res) => {
+  const lists = await List.findAll();
+  res.json({ lists });
+})
+);
 
-router.post()
+router.post("/", asyncHandler(async (req, res) => {
+  const {name} = req.body;
+  const list = db.List.build({
+    name,
+    userId: 1
+  })
+  await list.save()
+  res.redirect("/tasks")
+  //get userId from cookies
+})
+);
+
 module.exports = router;
