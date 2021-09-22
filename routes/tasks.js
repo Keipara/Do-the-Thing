661851850b2b8e54c82375c2ebae13130920689e
Nevin Chow <nevinchow@gmail.com>
2021-09-22 09:42:13 -0700

    dont merge

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,6 +4,15 @@ const db = require('../db/models')
 const { Task } = db;
 const {asyncHandler, handleValidationErrors} = require("./utils");
 
+
+
+const taskNotFoundError = (id) => {
+  const err = Error(`Task with id of ${id} could not be found.`);
+  err.title = "Task not found.";
+  err.status = 404;
+  return err;
+};
+
 router.get("/", asyncHandler(async (req, res) => {
     const tasks = await Task.findAll();
     res.render("tasks.pug")
@@ -14,9 +23,51 @@ router.get("/", asyncHandler(async (req, res) => {
 router.get("/task-list", asyncHandler(async (req, res) => {
   const tasks = await Task.findAll();
   res.json({ tasks });
+  //get userId to find taskList related to user. 
 })
 );
 
 
-router.post()
+router.post("/", asyncHandler(async (req, res) => {
+    const { name, complete, listId, createdAt, updatedAt} = req.body;
+    const task = await Task.create({ name, complete, listId, createdAt, updatedAt });
+    res.status(201).redirect('/tasks')
+  })
+);
+
+router.get("/:id(\\d+)", asyncHandler(async (req, res, next) => {
+    const taskId = parseInt(req.params.id, 10);
+    const task = await Task.findByPk(taskId);
+    res.json({ task });
+  })
+);
+
+router.patch("/:id(\\d+)", asyncHandler(async (req, res, next) => {
+    const taskId = parseInt(req.params.id, 10);
+    const task = await Task.findByPk(taskId);
+
+    if (task) {
+      await task.update({ name: req.body.name });
+      res.json({ task });
+    } else {
+      next(taskNotFoundError(taskId));
+    }
+  })
+);
+
+
+router.delete("/:id(\\d+)", asyncHandler(async (req, res, next) => {
+    const taskId = parseInt(req.params.id, 10);
+    const task = await Task.findByPk(taskId);
+
+    if (task) {
+      await task.destroy();
+      res.status(204).end();
+    } else {
+      next(taskNotFoundError(taskId));
+    }
+  })
+);
+
+
 module.exports = router;
