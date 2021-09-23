740e35e0fd0e430509d07c97a4d321c3bf00b76c
Your Name <email@example.com>
2021-09-23 11:01:44 -0700

    adding lists

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", async () => {
           `
           );
       taskContainer.innerHTML = taskHtml.join("");
-      
+
     } catch (e) {
       console.error(e);
     }
@@ -38,6 +38,28 @@ document.addEventListener("DOMContentLoaded", async () => {
         const { tasks } = await res.json();
         const taskContainer = document.querySelector(".task-list");
 
+
+      const listHtml = lists.map(
+          (list) =>
+          `
+          <div class="card">
+          <div class="card-body">
+          <p class="card-text">${list.name}</p>
+          </div>
+          </div>
+          `
+          );
+      listContainer.innerHTML = listHtml.join("");
+    } catch (e) {
+      console.error(e)
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", async() => {
+    try {
+      const res = await fetch("/tasks/list-list");
+      const { lists } = await res.json();
+      const listContainer = document.querySelector(".list-list");
 
       const listHtml = lists.map(
           (list) =>
